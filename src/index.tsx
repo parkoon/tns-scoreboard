c465081578cd3fc3883af1037021669fdc389565
parkoon <devparkoon@gmail.com>
2020-04-15 18:56:11 +0900

    rollback 🚗

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,9 @@ import GlobalProvider from './context/GlobalContext';
 
 ReactDOM.render(
     <React.StrictMode>
-        <HashRouter basename="/">
+        <HashRouter>
             <GlobalProvider>
-                <h1 style={{ fontSize: '20rem' }}>
-                    해당 사이트를 이용할 수 없습니다. 이용하시려면 박종혁님에게 문의 바랍니다.
-                </h1>
+                <App />
             </GlobalProvider>
         </HashRouter>
     </React.StrictMode>,
