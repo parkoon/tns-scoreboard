6a8d391bb22224f5a5922e89b25a92cfb0719ad4
parkoon <devparkoon@gmail.com>
2020-04-15 15:55:26 +0900

    Updates

---
+++ precache-manifest.315bb33d1eea34375fc04cd41d44e2c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849b7c371bf16d4ec9e28ac75200364e",
+    "url": "/tns-scoreboard/index.html"
+  },
+  {
+    "revision": "a37601a4136c86166de3",
+    "url": "/tns-scoreboard/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "36dad4701c2be36b3973",
+    "url": "/tns-scoreboard/static/css/main.ef8c15b4.chunk.css"
+  },
+  {
+    "revision": "a37601a4136c86166de3",
+    "url": "/tns-scoreboard/static/js/2.776aaf40.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tns-scoreboard/static/js/2.776aaf40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36dad4701c2be36b3973",
+    "url": "/tns-scoreboard/static/js/main.152f5006.chunk.js"
+  },
+  {
+    "revision": "fbbed7f2ba405f28c873",
+    "url": "/tns-scoreboard/static/js/runtime-main.317e3d2e.js"
+  }
+]);
